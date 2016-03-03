77f170411ee4ca5e734aabd26810bbb14ed0a845
sunriver <neymarjohn215@gmail.com>
2016-03-02 18:12:52 -0800

    [Android] Add drag events to event type constants. Closes #89

--- example/examples/DraggableMarkers.js
+++ example/examples/DraggableMarkers.js
@@ -52,11 +52,21 @@ var MarkerTypes = React.createClass({
             onSelect={(e) => console.log('onSelect', e)}
             onDrag={(e) => console.log('onDrag', e)}
             onDragStart={(e) => console.log('onDragStart', e)}
+            onDragEnd={(e) => console.log('onDragEnd', e)}
             onPress={(e) => console.log('onPress', e)}
             draggable
           >
             <PriceMarker amount={99} />
           </MapView.Marker>
+          <MapView.Marker
+            coordinate={this.state.b}
+            onSelect={(e) => console.log('onSelect', e)}
+            onDrag={(e) => console.log('onDrag', e)}
+            onDragStart={(e) => console.log('onDragStart', e)}
+            onDragEnd={(e) => console.log('onDragEnd', e)}
+            onPress={(e) => console.log('onPress', e)}
+            draggable
+          />
         </MapView>
       </View>
     );
