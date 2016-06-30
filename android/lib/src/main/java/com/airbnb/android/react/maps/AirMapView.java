3ba97acd1e2fdb9e985d8fb42b4c54090522ab7e
sunriver <neymarjohn215@gmail.com>
2016-06-29 20:51:06 -0700

    Fix shouldUsePinView

--- android/lib/src/main/java/com/airbnb/android/react/maps/AirMapView.java
+++ android/lib/src/main/java/com/airbnb/android/react/maps/AirMapView.java
@@ -529,19 +529,16 @@ public class AirMapView extends MapView implements GoogleMap.InfoWindowAdapter,
 
         switch (action) {
             case (MotionEvent.ACTION_DOWN):
-                this.getParent().requestDisallowInterceptTouchEvent(true);
                 isTouchDown = true;
                 break;
             case (MotionEvent.ACTION_MOVE):
                 startMonitoringRegion();
                 break;
             case (MotionEvent.ACTION_UP):
-                this.getParent().requestDisallowInterceptTouchEvent(false);
                 isTouchDown = false;
                 break;
         }
-        super.dispatchTouchEvent(ev);
-        return true;
+        return super.dispatchTouchEvent(ev);
     }
 
     // Timer Implementation
