6a9f5f2b23abca51bfba96931b120aa6a16296bf
sunriver <neymarjohn215@gmail.com>
2016-10-14 16:27:02 -0700

    Fixes crash during Activity onPause()

--- ios/AirGoogleMaps/AIRGoogleMapMarker.h
+++ ios/AirGoogleMaps/AIRGoogleMapMarker.h
@@ -26,7 +26,6 @@
 @property (nonatomic, copy) NSString *title;
 @property (nonatomic, copy) NSString *subtitle;
 @property (nonatomic, strong) UIColor *pinColor;
-@property (nonatomic, assign) NSInteger zIndex;
 @property (nonatomic, assign) BOOL draggable;
 
 - (void)showCalloutView;
