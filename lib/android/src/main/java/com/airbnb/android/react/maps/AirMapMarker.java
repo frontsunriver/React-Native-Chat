f1c994354cc9aa116d130f165f14b92557c5f1c6
sunriver <neymarjohn215@gmail.com>
2018-09-09 12:21:33 +0300

    Let onVisibilityChanged() be called on children (mainly fixes Image issues)

--- lib/android/src/main/java/com/airbnb/android/react/maps/AirMapMarker.java
+++ lib/android/src/main/java/com/airbnb/android/react/maps/AirMapMarker.java
@@ -73,9 +73,6 @@ public class AirMapMarker extends AirMapFeature {
   private float calloutAnchorY;
   private boolean calloutAnchorIsSet;
 
-  private boolean tracksViewChanges = true;
-  private boolean tracksViewChangesActive = false;
-
   private boolean hasCustomMarkerView = false;
 
   private final DraweeHolder<?> logoHolder;
@@ -243,32 +240,6 @@ public class AirMapMarker extends AirMapFeature {
     update();
   }
 
-  public void setTracksViewChanges(boolean tracksViewChanges) {
-    this.tracksViewChanges = tracksViewChanges;
-    updateTracksViewChanges();
-  }
-
-  private void updateTracksViewChanges() {
-    boolean shouldTrack = tracksViewChanges && hasCustomMarkerView && marker != null;
-    if (shouldTrack == tracksViewChangesActive) return;
-    tracksViewChangesActive = shouldTrack;
-
-    if (shouldTrack) {
-      ViewChangesTracker.getInstance().addMarker(this);
-    } else {
-      ViewChangesTracker.getInstance().removeMarker(this);
-    }
-  }
-
-  public boolean updateCustomMarkerIcon() {
-    if (!tracksViewChangesActive)
-      return false;
-
-    marker.setIcon(getIcon());
-
-    return true;
-  }
-
   public LatLng interpolate(float fraction, LatLng a, LatLng b) {
     double lat = (b.latitude - a.latitude) * fraction + a.latitude;
     double lng = (b.longitude - a.longitude) * fraction + a.longitude;
@@ -342,7 +313,6 @@ public class AirMapMarker extends AirMapFeature {
     // if children are added, it means we are rendering a custom marker
     if (!(child instanceof AirMapCallout)) {
       hasCustomMarkerView = true;
-      updateTracksViewChanges();
     }
     update();
   }
@@ -355,14 +325,12 @@ public class AirMapMarker extends AirMapFeature {
   @Override
   public void addToMap(GoogleMap map) {
     marker = map.addMarker(getMarkerOptions());
-    updateTracksViewChanges();
   }
 
   @Override
   public void removeFromMap(GoogleMap map) {
     marker.remove();
     marker = null;
-    updateTracksViewChanges();
   }
 
   private BitmapDescriptor getIcon() {
